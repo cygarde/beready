6ff4477ec02fba2b043b6a1685bf764c3ae8bff8
Mathieu Dassie <dassie.mathieu@gmail.com>
2020-05-23 17:15:16 +0200

    model_Review

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :product
   belongs_to :user
-
+  has_many :reviews
   validates :start_date, presence: true
   validates :end_date, presence: true
 end

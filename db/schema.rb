421ff507894de87301f5d25e4008e9fd22c85df8
kamel <k.moussaoui@simply-permis.com>
2020-05-27 00:10:57 +0200

    add bookings controller and routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_174822) do
+ActiveRecord::Schema.define(version: 2020_05_26_213619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.datetime "start_date"
-    t.datetime "end_date"
+    t.date "start_date"
+    t.date "end_date"
     t.bigint "product_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -33,9 +33,10 @@ ActiveRecord::Schema.define(version: 2020_05_25_174822) do
     t.integer "unit_price"
     t.string "category"
     t.string "city"
-    t.integer "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -68,5 +69,6 @@ ActiveRecord::Schema.define(version: 2020_05_25_174822) do
 
   add_foreign_key "bookings", "products"
   add_foreign_key "bookings", "users"
+  add_foreign_key "products", "users"
   add_foreign_key "reviews", "bookings"
 end

dc1c85d2cfe146f8c18a9841ed35101514a70d29
Cyril Garde <cygarde@free.fr>
2020-05-30 11:42:22 +0200

    correction + ajout index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  skip_before_action :authenticate_user! only: [:home]
+  skip_before_action :authenticate_user!, only: [:index]
 
 before_action :set_product, only: [:show, :edit, :destroy]
 

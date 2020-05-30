6fde20770206f5cf541433934d340fbb4c7f4d96
laura mairot <lauramairot@gmail.com>
2020-05-30 13:57:20 +0200

    modif_title_new_product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  skip_before_action :authenticate_user! only: [:home]
+  skip_before_action :authenticate_user!, only: [:home]
 
 before_action :set_product, only: [:show, :edit, :destroy]
 

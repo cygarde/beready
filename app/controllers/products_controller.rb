79bc2efb8afc9bf39ad8a6af7bd84b476bb5ff93
laura mairot <lauramairot@gmail.com>
2020-05-30 11:17:38 +0200

    add_some_front

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user! only: [:home]
+  #skip_before_action :authenticate_user! only: [:home]
 before_action :set_product, only: [:show, :edit, :destroy]
 
   def index

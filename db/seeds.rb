42ac563201a2729a60f950774f568fe33b3ff857
Mathieu Dassie <dassie.mathieu@gmail.com>
2020-06-01 19:34:55 +0200

    fichiermodif

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,7 @@ puts 'Creating products'
 user = User.create(email: "laura@gmail.com", password: "123456")
 
 backpack = Product.new(name: "Sac à dos", description: "20L 4 poches", state: 3, category: "Randonnée", unit_price: 15, city: "Marseille")
+
 backpack.user = user
 backpack.save
 
